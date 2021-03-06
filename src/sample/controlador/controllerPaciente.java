0379e8102d0a6e84730a57b8fde31b6f33555fef
LuchoAlm <58267390+LuchoAlm@users.noreply.github.com>
2021-03-06 09:42:08 -0500

    Add files via upload

---
+++ src/sample/controlador/controllerPaciente.java
@@ -0,0 +1,186 @@
+package sample.controlador;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import org.kordamp.ikonli.javafx.FontIcon;
+import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class controllerPaciente implements Initializable {
+    @FXML
+    //Botones de la barra de menú principal
+    private FontIcon exit, menu, minimice, regresar;
+
+    @FXML
+    //Icono de los botones del menú lateral
+    private FontIcon icoSucursal, icoPaciente, icoOdontologo, icoCita, icoHistoriaClinica, icoCatalogo;
+
+    @FXML
+    //Panel de menú lateral
+    private AnchorPane pane1, pane2;
+
+    @FXML
+    //Botones del menú lateral
+    private JFXButton btnSucursal, btnPaciente, btnOdontologo, btnCita, btnHsitoriaClinica, btnCatalogo, btnRegresar;
+
+    //Elementos propios de la ventana
+    @FXML
+    private FontIcon regresarOpciones;
+    @FXML
+    private AnchorPane paneOpciones, paneRegistroPaciente, paneConsultarPaciente;
+    @FXML
+    private JFXButton btnRegistrarPaciente,btnConsultarPaciente, btnActualizarPaciente, btnEliminarPaciente;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //Para cerrar la ventana
+        exit.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+
+        //Para minimazar la ventana
+        minimice.setOnMouseClicked(event -> {
+
+            Stage s;
+            s = (Stage) ((FontIcon) event.getSource()).getScene().getWindow();
+            s.setIconified(true);
+        });
+
+        //Para regrezar al Login Principal
+        regresar.setOnMouseClicked(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../vista/menuPrincipal.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.setScene(scene);
+                stage.show();
+
+                Stage s;
+                s = (Stage) ((FontIcon) event.getSource()).getScene().getWindow();
+                s.close();
+            } catch (IOException e) {
+                System.out.println("Error al cargar de Login");
+            }
+        });
+
+        btnRegresar.setOnMouseClicked(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../vista/menuPrincipal.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.setScene(scene);
+                stage.show();
+                Stage s;
+                s = (Stage) ((JFXButton) event.getSource()).getScene().getWindow();
+                s.close();
+            } catch (IOException e) {
+                System.out.println("Error al cargar la ventana");
+            }
+        });
+
+        btnRegresar.setOnMouseClicked(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../vista/menuPrincipal.fxml"));
+                Parent root = loader.load();
+
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.setScene(scene);
+                stage.show();
+
+                Stage s;
+                s = (Stage) ((JFXButton) event.getSource()).getScene().getWindow();
+                s.close();
+            } catch (IOException e) {
+                System.out.println("Error al cargar de Login");
+            }
+        });
+
+        //Lógica para el desplazamiento de los menús
+
+        pane1.setVisible(false);
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.35), pane1);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.35), pane2);
+        translateTransition.setByX(-600);
+        translateTransition.play();
+
+        menu.setOnMouseClicked(event -> {
+
+            pane1.setVisible(true);
+            menu.setVisible(false);
+
+            FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.35), pane1);
+            fadeTransition1.setFromValue(0);
+            fadeTransition1.setToValue(1.0);
+            fadeTransition1.play();
+
+            TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.35), pane2);
+            translateTransition1.setByX(+600);
+            translateTransition1.play();
+        });
+
+        pane1.setOnMouseClicked(event -> {
+            FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(0.35), pane1);
+            fadeTransition1.setFromValue(1.0);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(0.35), pane2);
+            translateTransition1.setByX(-600);
+            translateTransition1.play();
+            menu.setVisible(true);
+            pane1.setVisible(false);
+        });
+
+
+        //Lógica de la ventana
+
+        regresarOpciones.setOnMouseClicked(event -> {
+            if (paneConsultarPaciente.isVisible() == true){
+                paneConsultarPaciente.setVisible(false);
+            }
+            else if(paneRegistroPaciente.isVisible() == true){
+                paneRegistroPaciente.setVisible(false);
+            }
+            regresarOpciones.setVisible(false);
+            regresar.setVisible(true);
+            paneOpciones.setVisible(true);
+        });
+
+        btnRegistrarPaciente.setOnMouseClicked(event -> {
+            paneOpciones.setVisible(false);
+            paneRegistroPaciente.setVisible(true);
+            regresar.setVisible(false);
+            regresarOpciones.setVisible(true);
+        });
+
+        btnConsultarPaciente.setOnMouseClicked(event -> {
+            paneOpciones.setVisible(false);
+            paneConsultarPaciente.setVisible(true);
+            regresar.setVisible(false);
+            regresarOpciones.setVisible(true);
+        });
+    }
+}
